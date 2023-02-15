389149aa46fa9e7bb3d1ab6959e4d92f91060d43
Hayden Scaggs <haydenkicks12@gmail.com>
2023-02-15 12:33:29 -0700

    prepare

--- prepare/Learning04/Program.cs
+++ prepare/Learning04/Program.cs
@@ -1,9 +1,21 @@
-using System;
-
-class Program
+public class WritingAssignment : Assignment
 {
-    static void Main(string[] args)
+    private string _title;
+
+    // Notice the syntax here that the WritingAssignment constructor has 3 parameters and then
+    // it passes 2 of them directly to the "base" constructor, which is the "Assignment" class constructor.
+    public WritingAssignment(string studentName, string topic, string title)
+        : base(studentName, topic)
     {
-        Console.WriteLine("Hello Learning04 World!");
+        // Here we set any variables specific to the WritingAssignment class
+        _title = title;
+    }
+
+    public string GetWritingInformation()
+    {
+        // Notice that we are calling the getter here because _studentName is private in the base class
+        string studentName = GetStudentName();
+
+        return $"{_title} by {studentName}";
     }
 }
